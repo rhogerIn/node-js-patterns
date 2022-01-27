5d1a86fe46f6258fb4b864efe7e6a7b509af1e6c
Rogério Souza <67068084+rhogerIn@users.noreply.github.com>
2022-01-27 08:54:46 -0300

    feat: test data builder and mother object patterns

---
+++ creational patterns/test-data-builder/test/productValdator-data-builder.test.js
@@ -0,0 +1,21 @@
+const { expect } = require('chai')
+const { it, describe } = require('mocha')
+
+const { productValidator } = require('../src')
+const ProductDataBuilder = require('./model/productDataBuilder')
+
+
+describe('Test Data Builder', () => {
+/*     it('shouldn\'t return error with valid product', () => {
+        const product = new ProductDataBuilder.aProduct().build() 
+
+        const result = productValidator(product)
+
+        const expected = {
+            errors: [],
+            result: true
+        }
+
+        expect(result).to.be.deep.equal(expected)
+    }) */
+}) 
